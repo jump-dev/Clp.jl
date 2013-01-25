307bab1c84acf5a553e56f2dccd098cc6bafb568
Miles Lubin <miles.lubin@gmail.com>
2013-01-25 02:12:43 -0500

    Use julia's blas and lapack

--- deps/build.jl
+++ deps/build.jl
@@ -1,13 +1,14 @@
 println("WARNING: This build script has not been extensively tested. Please report any issues.")
 clpname = "Clp-1.14.8"
 prefix = joinpath(Pkg.dir(),"Clp","deps","usr")
+libdir = joinpath(JULIA_HOME,"..","lib")
 if !isfile("$clpname.tgz")
     run(`wget http://www.coin-or.org/download/source/Clp/$clpname.tgz`)
     run(`tar xvzf $clpname.tgz`)
     cd("$clpname")
     run(`cat ../Clp-makefile.patch` | `patch -p1`)
     run(`cat ../Clp-interface.patch` | `patch -p0`)
-    run(`./configure --prefix=$prefix`)
+    run(`./configure --prefix=$prefix --with-blas="-L$libdir -lopenblas" --with-lapack=`)
     run(`make install`)
 end
 
