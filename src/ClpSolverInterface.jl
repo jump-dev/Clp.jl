f377fe126c01014ce81d522ef5e6597675222178
Issam Tahiri <issam.tahiri@inria.fr>
2017-08-04 14:30:20 +0200

    wip: more functions

--- src/ClpSolverInterface.jl
+++ src/ClpSolverInterface.jl
@@ -255,6 +255,25 @@ immutable ClpMOISolver <: MOI.AbstractSolver
 end
 ClpMOISolver(;kwargs...) = ClpMOISolver(kwargs)
 
+function MOI.supportsproblem(s::ClpMOISolver, objective_type, constraint_types::Vector)
+    if objective_type != MOI.ScalarAffineFunction{Float64}
+        return false
+    end
+    supported_constraint_types = [(MOI.ScalarAffineFunction{Float64}, MOI.LessThan{Float64}),
+            (MOI.ScalarAffineFunction{Float64}, MOI.GreaterThan{Float64}),
+            (MOI.ScalarAffineFunction{Float64}, MOI.Interval{Float64}),
+            (MOI.SingleVariable, MOI.LessThan{Float64}),
+            (MOI.SingleVariable, MOI.GreaterThan{Float64})]
+    for v in constraint_types
+        if !(v in supported_constraint_types)
+            return false
+        end
+    end
+    return true
+end
+
+MOI.getattribute(s::ClpMOISolver, ::MOI.SupportsDuals) = true
+
 # Solver instance
 
 const VR = MOI.VariableReference
@@ -268,15 +287,25 @@ type ClpMOIModel <: MOI.AbstractSolverInstance
     vartoubref::Vector{CR}
     ubreftovar::Dict{CR, Int32}
     lbreftovar::Dict{CR, Int32}
+    nupperbounds::UInt64
+    nlowerbounds::UInt64
     nconstraints::UInt64 # does take into account bounds
     # constraints::Vector{CR}
-    nrows::UInt64 # does not take into account bounds
+    nrows_greaterthan::UInt64
+    nrows_lessthan::UInt64
+    nrows_interval::UInt64
+    nrows::UInt64
     rowtorangeref::Vector{CR}
     rangereftorow::Dict{CR, Int32}
+    solution::Vector{Float64}
+    rowsolution::Vector{Float64}
+    dualcolsolution::Vector{Float64}
+    dualrowsolution::Vector{Float64}
 end
 function ClpMOIModel(;kwargs...)
     m = ClpMOIModel(ClpModel(),ClpSolve(), 0, Vector{CR}(), Vector{CR}(), Dict{CR, Int32}(), Dict{CR, Int32}(),
-            0, 0, Vector{CR}(), Dict{CR, Int32}() )
+            0, 0, 0, 0, 0, 0, 0, Vector{CR}(), Dict{CR, Int32}(),
+            Vector{Float64}(), Vector{Float64}(), Vector{Float64}(), Vector{Float64}() )
     set_log_level(m.inner,0) # disable output by default
     for (name, value) in kwargs
         setoption(m, name, value)
@@ -285,7 +314,7 @@ function ClpMOIModel(;kwargs...)
 end
 
 col(m::ClpMOIModel,ref::VR) = ref.value - 1 # column index in solver
-row(m::ClpMOIModel,ref::CR) = rangereftorow[ref.value] - 1 # row index in solver
+row(m::ClpMOIModel,ref::CR) = m.rangereftorow[ref] - 1 # row index in solver
 
 MOI.SolverInstance(s::ClpMOISolver) = ClpMOIModel(;s.options...)
 
@@ -301,14 +330,14 @@ function MOI.addvariable!(m::ClpMOIModel)
     m.nconstraints += 2
     nv = m.nvariables
     nc = m.nconstraints
-    vf = VR(nv)
-    lbref = CR{MOI.ScalarVariablewiseFunction, MOI.LessThan{Float64}}(nc - 1)
-    ubref = CR{MOI.ScalarVariablewiseFunction, MOI.GreaterThan{Float64}}(nc)
+    vref = VR(nv)
+    lbref = CR{MOI.SingleVariable, MOI.GreaterThan{Float64}}(nc - 1)
+    ubref = CR{MOI.SingleVariable, MOI.LessThan{Float64}}(nc)
     push!(m.vartolbref, lbref)
     push!(m.vartoubref, ubref)
     m.lbreftovar[lbref] = nv
     m.ubreftovar[ubref] = nv
-    return vf
+    return vref
 end
 
 function MOI.addvariables!(m::ClpMOIModel, n::Integer)
@@ -341,18 +370,36 @@ function MOI.setobjective!(m::ClpMOIModel, sense::MOI.OptimizationSense, f::MOI.
     end
 end
 
-# MOI.getattribute(m::ClpMOIModel, ::MOI.Sense)
+function MOI.getattribute(m::ClpMOIModel, ::MOI.ObjectiveSense)
+    s = get_obj_sense(m.inner)
+    if s == 1.0
+        return MOI.MinSense
+    elseif s == -1.0
+        return MOI.MaxSense
+    else
+        error("library get_obj_sense returned an incorrect value")
+    end
+end
+
 # MOI.getattribute(m::ClpMOIModel, ::MOI.ObjectiveFunction)
 # MOI.modifyobjective!(m::ClpMOIModel, change::MOI.AbstractFunctionModification)
 
 # Constraint
-function _addrangeref!(F,S,m::ClpMOIModel)
+function _addrangeref!(F,S,m::ClpMOIModel, haslessthan::Bool, hasgreaterthan::Bool)
+    @assert haslessthan || hasgreaterthan
     m.nconstraints += 1
     m.nrows += 1
-    cf = CR{F,S}(m.nconstraints)
-    push!(m.rowtorangeref, cf)
-    m.rangereftorow[cf] = m.nrows
-    return cf
+    if !haslessthan
+        m.nrows_lessthan += 1
+    elseif !hasgreaterthan
+        m.nrows_greaterthan += 1
+    else
+        m.nrows_interval += 1
+    end
+    cref = CR{F,S}(m.nconstraints)
+    push!(m.rowtorangeref, cref)
+    m.rangereftorow[cref] = m.nrows
+    return cref
 end
 
 function _addrangeinsolver!(m::ClpMOIModel, f::MOI.ScalarAffineFunction{Float64}, lower::Float64, upper::Float64)
@@ -366,21 +413,22 @@ function _addrangeinsolver!(m::ClpMOIModel, f::MOI.ScalarAffineFunction{Float64}
     add_row(m.inner, number_in_row, columns, elements, lower - f.constant, upper - f.constant)
 end
 
-function _addrange!(m::ClpMOIModel, f::MOI.ScalarAffineFunction{Float64}, lower::Float64, upper::Float64)
+function _addrange!(m::ClpMOIModel, f::MOI.ScalarAffineFunction{Float64}, lower::Float64, upper::Float64,
+        haslower::Bool, hasupper::Bool)
     _addrangeinsolver!(m, f, lower, upper)
-    return _addrangeref!(MOI.ScalarAffineFunction{Float64},MOI.Interval{Float64},m)
+    return _addrangeref!(MOI.ScalarAffineFunction{Float64},MOI.Interval{Float64},m, haslower, hasupper)
 end
 
 function MOI.addconstraint!(m::ClpMOIModel, f::MOI.ScalarAffineFunction{Float64}, s::MOI.Interval{Float64})
-    return _addrange!(m, f, s.lower, s.upper)
+    return _addrange!(m, f, s.lower, s.upper, true, true)
 end
 
 function MOI.addconstraint!(m::ClpMOIModel, f::MOI.ScalarAffineFunction{Float64}, s::MOI.GreaterThan{Float64})
-    return _addrange!(m, f, s.lower, Inf)
+    return _addrange!(m, f, s.lower, Inf, true, false)
 end
 
 function MOI.addconstraint!(m::ClpMOIModel, f::MOI.ScalarAffineFunction{Float64}, s::MOI.LessThan{Float64})
-    return _addrange!(m, f, -Inf, s.upper)
+    return _addrange!(m, f, -Inf, s.upper, false, true)
 end
 
 function replaceInf(x)
@@ -394,18 +442,45 @@ function replaceInf(x)
     return x
 end
 
-function MOI.addconstraint!(m::ClpMOIModel, f::MOI.ScalarVariablewiseFunction, s::MOI.GreaterThan{<:Real})
+function MOI.addconstraint!(m::ClpMOIModel, f::MOI.SingleVariable, s::MOI.GreaterThan{<:Real})
     lowerbounds = replaceInf(get_col_lower(m.inner))
     lowerbounds[col(m, f.variable) + 1] = s.lower
+    m.nlowerbounds += 1
     chg_column_lower(m.inner, lowerbounds)
     return m.vartolbref[col(m, f.variable) + 1]
 end
 
-function MOI.addconstraint!(m::ClpMOIModel, f::MOI.ScalarVariablewiseFunction, s::MOI.LessThan{<:Real})
+function MOI.addconstraint!(m::ClpMOIModel, f::MOI.SingleVariable, s::MOI.LessThan{<:Real})
     upperbounds = replaceInf(get_col_upper(m.inner))
-    upperbounds[col(m, f.variable) + 1] = s.lower
+    upperbounds[col(m, f.variable) + 1] = s.upper
+    m.nupperbounds += 1
     chg_column_upper(m.inner, upperbounds)
     return m.vartoubref[col(m, f.variable) + 1]
+end
+
+function MOI.getattribute(m::ClpMOIModel,
+        ::MOI.NumberOfConstraints{MOI.SingleVariable, MOI.LessThan{Float64}})
+    return m.nupperbounds
+end
+
+function MOI.getattribute(m::ClpMOIModel,
+        ::MOI.NumberOfConstraints{MOI.SingleVariable, MOI.GreaterThan{Float64}})
+    return m.nlowerbounds
+end
+
+function MOI.getattribute(m::ClpMOIModel,
+        ::MOI.NumberOfConstraints{MOI.ScalarAffineFunction{Float64},MOI.LessThan{Float64}})
+    return m.nrows_lessthan
+end
+
+function MOI.getattribute(m::ClpMOIModel,
+        ::MOI.NumberOfConstraints{MOI.ScalarAffineFunction{Float64},MOI.GreaterThan{Float64}})
+    return m.nrows_greaterthan
+end
+
+function MOI.getattribute(m::ClpMOIModel,
+        ::MOI.NumberOfConstraints{MOI.ScalarAffineFunction{Float64},MOI.Interval{Float64}})
+    return m.nrows_interval
 end
 
 # MOI.delete!(m::ClpMOIModel, cr::CR)
@@ -413,18 +488,112 @@ end
 
 # Optimization and additional attributes
 function MOI.optimize!(m::ClpMOIModel)
-    initial_solve(m.inner)
+    initial_solve_with_options(m.inner,m.solveroptions)
+    m.solution = primal_column_solution(m.inner)
+    m.rowsolution = primal_row_solution(m.inner)
+    m.dualcolsolution = dual_column_solution(m.inner) .* get_obj_sense(m.inner)
+    m.dualrowsolution = dual_row_solution(m.inner) .* get_obj_sense(m.inner)
 end
 
-# MOI.getattribute(m::ClpMOIModel, noc::MOI.NumberOfConstraints)
-# MOI.getattribute(m::ClpMOIModel, loc::MOI.ListOfConstraints)
-# MOI.cangetattribute(m::ClpMOIModel, ::Union{MOI.NumberOfVariables,
-#                                                  MOI.NumberOfConstraints,
-#                                                  MOI.ListOfConstraints,
-#                                                  MOI.ObjectiveFunction,
-#                                                  MOI.Sense}) = true
-# MOI.cangetattribute(m::ClpMOIModel, ::Union{MOI.ConstraintFunction,
-#                                                  MOI.ConstraintSet}, ref) = true
+function MOI.getattribute(m::ClpMOIModel, ::MOI.TerminationStatus)
+    s = ClpCInterface.status(m.inner)
+    if s == 0
+        return MOI.Success
+    elseif s == 1
+        return MOI.InfeasibleNoResult
+    elseif s == 2
+        return MOI.UnboundedNoResult
+    elseif s == 3
+        return MOI.OtherLimit
+    elseif s == 4
+        return MOI.OtherError
+    else
+        error("Internal library error")
+    end
+end
+
+function MOI.getattribute(m::ClpMOIModel, ::MOI.PrimalStatus)
+    if primal_feasible(m.inner)
+        return MOI.FeasiblePoint
+    else
+        return MOI.UnknownResultStatus
+    end
+end
+
+function MOI.getattribute(m::ClpMOIModel, ::MOI.DualStatus)
+    if dual_feasible(m.inner)
+        return MOI.FeasiblePoint
+    else
+        return MOI.UnknownResultStatus
+    end
+end
+
+MOI.getattribute(m::ClpMOIModel, ::MOI.ObjectiveValue) = objective_value(m.inner)
+
+function MOI.getattribute(m::ClpMOIModel, ::MOI.VariablePrimal, vref::VR)
+    return m.solution[col(m,vref)+1]
+end
+
+function MOI.getattribute(m::ClpMOIModel, ::MOI.VariablePrimal, vec::Vector{VR})
+    return [m.solution[col(m,vref)+1] for vref in vec]
+end
+
+function MOI.getattribute(m::ClpMOIModel, ::MOI.ConstraintPrimal, cref::CR)
+    return m.rowsolution[row(m,cref)+1]
+end
+
+function MOI.getattribute(m::ClpMOIModel, ::MOI.ConstraintPrimal, vec::Vector{CR})
+    return [m.rowsolution[row(m,cref)+1] for cref in vec]
+end
+
+function MOI.getattribute(m::ClpMOIModel, ::MOI.ConstraintDual, cref::CR{MOI.ScalarAffineFunction{Float64},S}
+        where S <: Union{MOI.GreaterThan{Float64}, MOI.LessThan{Float64}, MOI.Interval{Float64}})
+    return m.dualrowsolution[row(m,cref)+1]
+end
+
+function MOI.getattribute(m::ClpMOIModel, ::MOI.ConstraintDual,
+        cref::CR{MOI.SingleVariable, MOI.GreaterThan{Float64}})
+    var = m.lbreftovar[cref]
+    if m.solution[var] == get_col_lower(m.inner)[var] #this wouold be more efficient to get from instance
+        return m.dualcolsolution[var]
+    else
+        return 0
+    end
+end
+
+function MOI.getattribute(m::ClpMOIModel, ::MOI.ConstraintDual,
+        cref::CR{MOI.SingleVariable, MOI.LessThan{Float64}})
+    var = m.ubreftovar[cref]
+    if m.solution[var] == get_col_upper(m.inner)[var] #this wouold be more efficient to get from instance
+        return m.dualcolsolution[var]
+    else
+        return 0
+    end
+end
+
+function MOI.getattribute(m::ClpMOIModel, ::MOI.ConstraintDual, vec::Vector{CR})
+    return [MOI.getattribute(m, MOI.ConstraintDual(), cref) for cref in vec]
+end
+
+MOI.cangetattribute(m::ClpMOIModel, ::Union{MOI.TerminationStatus,
+                                    MOI.PrimalStatus,
+                                    MOI.DualStatus,
+                                    MOI.NumberOfVariables,
+                                    MOI.NumberOfConstraints,
+                                    MOI.ObjectiveSense,
+                                    MOI.ObjectiveValue
+                                    }) = true
+
+MOI.cangetattribute(m::ClpMOIModel, ::Union{MOI.ConstraintPrimal, MOI.VariablePrimal, MOI.ConstraintDual},
+        ::Union{VR, CR}) = true
+
+MOI.cangetattribute(m::ClpMOIModel, ::Union{MOI.ConstraintPrimal, MOI.VariablePrimal},
+        ::Union{Vector{VR}, Vector{CR}}) = true
+
+MOI.cangetattribute(m::ClpMOIModel, ::MOI.ListOfConstraints) = false
+MOI.cangetattribute(m::ClpMOIModel, ::Union{MOI.ConstraintFunction,
+                                                 MOI.ConstraintSet}, ref::Union{VR, CR}) = false
+MOI.cangetattribute(m::ClpMOIModel, ::MOI.ObjectiveFunction) = false
 # MOI.getattribute(m::ClpMOIModel, ::MOI.ConstraintFunction, cr::CR)
 # MOI.getattribute(m::ClpMOIModel, ::MOI.ConstraintSet, cr::CR)
 
--- test/mathoptinterface.jl
+++ test/mathoptinterface.jl
@@ -2,29 +2,141 @@ using MathOptInterface, Clp, Base.Test
 const MOI = MathOptInterface
 
 solver = ClpMOISolver()
+ε=Base.rtoldefault(Float64)
 
-# @test MOI.supportsproblem(solver, MOI.ScalarAffineFunction{Float64}, [(MOI.ScalarAffineFunction{Float64},MOI.LessThan{Float64}),(MOI.ScalarVariablewiseFunction,MOI.GreaterThan{Float64})])
+function linear1test(solver::MOI.AbstractSolver, ε=Base.rtoldefault(Float64))
+    @testset "Basic solve, query, resolve" begin
+        # simple 2 variable, 1 constraint problem
+        # min -x
+        # st   x + y <= 1   (x + y - 1 ∈ Nonpositives)
+        #       x, y >= 0   (x, y ∈ Nonnegatives)
 
-m = MOI.SolverInstance(solver)
+        @test MOI.supportsproblem(solver, MOI.ScalarAffineFunction{Float64}, [(MOI.ScalarAffineFunction{Float64},MOI.LessThan{Float64}),(MOI.SingleVariable,MOI.GreaterThan{Float64})])
 
-v = MOI.addvariables!(m, 2)
-@test MOI.getattribute(m, MOI.NumberOfVariables()) == 2
+        m = MOI.SolverInstance(solver)
 
-cf = MOI.ScalarAffineFunction(v, [1.0,1.0], 0.0)
-c = MOI.addconstraint!(m, cf, MOI.LessThan(1.0))
+        v = MOI.addvariables!(m, 2)
+        @test MOI.getattribute(m, MOI.NumberOfVariables()) == 2
 
-# @test MOI.getattribute(m, MOI.NumberOfConstraints{MOI.ScalarAffineFunction{Float64},MOI.LessThan{Float64}}()) == 1
+        cf = MOI.ScalarAffineFunction(v, [1.0,1.0], 0.0)
+        c = MOI.addconstraint!(m, cf, MOI.LessThan(1.0))
+        @test MOI.getattribute(m, MOI.NumberOfConstraints{MOI.ScalarAffineFunction{Float64},MOI.LessThan{Float64}}()) == 1
 
-vc1 = MOI.addconstraint!(m, MOI.ScalarVariablewiseFunction(v[1]), MOI.GreaterThan(0.0))
-vc2 = MOI.addconstraint!(m, MOI.ScalarVariablewiseFunction(v[2]), MOI.GreaterThan(0.0))
+        vc1 = MOI.addconstraint!(m, MOI.SingleVariable(v[1]), MOI.GreaterThan(0.0))
+        vc2 = MOI.addconstraint!(m, MOI.SingleVariable(v[2]), MOI.GreaterThan(0.0))
+        @test MOI.getattribute(m, MOI.NumberOfConstraints{MOI.SingleVariable,MOI.GreaterThan{Float64}}()) == 2
 
-# @test MOI.getattribute(m, MOI.NumberOfConstraints{MOI.ScalarAffineFunction{Float64},MOI.GreaterThan{Float64}}()) == 2
+        objf = MOI.ScalarAffineFunction(v, [-1.0,0.0], 0.0)
+        MOI.setobjective!(m, MOI.MinSense, objf)
 
-objf = MOI.ScalarAffineFunction(v, [-1.0,0.0], 0.0)
-MOI.setobjective!(m, MOI.MinSense, objf)
+        @test MOI.getattribute(m, MOI.ObjectiveSense()) == MOI.MinSense
 
-# @test MOI.getattribute(m, MOI.Sense()) == MOI.MinSense
+        if MOI.cangetattribute(m, MOI.ObjectiveFunction())
+            @test objf ≈ MOI.getattribute(m, MOI.ObjectiveFunction())
+        end
 
-@show Clp.ClpCInterface.get_constraint_matrix(m.inner)
+        if MOI.cangetattribute(m, MOI.ConstraintFunction(), c)
+            @test cf ≈ MOI.getattribute(m, MOI.ConstraintFunction(), c)
+        end
 
-# @show Clp.ClpCInterface.print_model(m.inner, "testlp")
+        if MOI.cangetattribute(m, MOI.ConstraintSet(), c)
+            s = MOI.getattribute(m, MOI.ConstraintSet(), c)
+            @test s == MOI.LessThan(1.0)
+        end
+
+        if MOI.cangetattribute(m, MOI.ConstraintSet(), vc1)
+            s = MOI.getattribute(m, MOI.ConstraintSet(), vc1)
+            @test s == MOI.GreaterThan(0.0)
+        end
+        if MOI.cangetattribute(m, MOI.ConstraintSet(), vc2)
+            s = MOI.getattribute(m, MOI.ConstraintSet(), vc2)
+            @test s == MOI.GreaterThan(0.0)
+        end
+
+        MOI.optimize!(m)
+
+        @test MOI.cangetattribute(m, MOI.TerminationStatus())
+        @test MOI.getattribute(m, MOI.TerminationStatus()) == MOI.Success
+
+        @test MOI.cangetattribute(m, MOI.PrimalStatus())
+        @test MOI.getattribute(m, MOI.PrimalStatus()) == MOI.FeasiblePoint
+
+        @test MOI.cangetattribute(m, MOI.ObjectiveValue())
+        @test MOI.getattribute(m, MOI.ObjectiveValue()) ≈ -1 atol=ε
+
+        @test MOI.cangetattribute(m, MOI.VariablePrimal(), v)
+        @test MOI.getattribute(m, MOI.VariablePrimal(), v) ≈ [1, 0] atol=ε
+
+        @test MOI.cangetattribute(m, MOI.ConstraintPrimal(), c)
+        @test MOI.getattribute(m, MOI.ConstraintPrimal(), c) ≈ 1 atol=ε
+
+        if MOI.getattribute(solver, MOI.SupportsDuals())
+            @test MOI.cangetattribute(m, MOI.DualStatus())
+            @test MOI.getattribute(m, MOI.DualStatus()) == MOI.FeasiblePoint
+            @test MOI.cangetattribute(m, MOI.ConstraintDual(), c)
+            @test MOI.getattribute(m, MOI.ConstraintDual(), c) ≈ -1 atol=ε
+
+            # reduced costs
+            @test MOI.cangetattribute(m, MOI.ConstraintDual(), vc1)
+            @test MOI.getattribute(m, MOI.ConstraintDual(), vc1) ≈ 0 atol=ε
+            @test MOI.cangetattribute(m, MOI.ConstraintDual(), vc2)
+            @test MOI.getattribute(m, MOI.ConstraintDual(), vc2) ≈ 1 atol=ε
+        end
+
+        # change objective to Max +x
+
+        objf = MOI.ScalarAffineFunction(v, [1.0,0.0], 0.0)
+        MOI.setobjective!(m, MOI.MaxSense, objf)
+
+        if MOI.cangetattribute(m, MOI.ObjectiveFunction())
+            @test objf ≈ MOI.getattribute(m, MOI.ObjectiveFunction())
+        end
+
+        @test MOI.getattribute(m, MOI.ObjectiveSense()) == MOI.MaxSense
+
+        MOI.optimize!(m)
+
+        @test MOI.cangetattribute(m, MOI.TerminationStatus())
+        @test MOI.getattribute(m, MOI.TerminationStatus()) == MOI.Success
+
+        @test MOI.cangetattribute(m, MOI.PrimalStatus())
+        @test MOI.getattribute(m, MOI.PrimalStatus()) == MOI.FeasiblePoint
+
+        @test MOI.cangetattribute(m, MOI.ObjectiveValue())
+        @test MOI.getattribute(m, MOI.ObjectiveValue()) ≈ 1 atol=ε
+
+        @test MOI.cangetattribute(m, MOI.VariablePrimal(), v)
+        @test MOI.getattribute(m, MOI.VariablePrimal(), v) ≈ [1, 0] atol=ε
+
+        if MOI.getattribute(solver, MOI.SupportsDuals())
+            @test MOI.cangetattribute(m, MOI.DualStatus())
+            @test MOI.getattribute(m, MOI.DualStatus()) == MOI.FeasiblePoint
+            @test MOI.cangetattribute(m, MOI.ConstraintDual(), c)
+            @test MOI.getattribute(m, MOI.ConstraintDual(), c) ≈ -1 atol=ε
+
+            @test MOI.cangetattribute(m, MOI.ConstraintDual(), vc1)
+            @test MOI.getattribute(m, MOI.ConstraintDual(), vc1) ≈ 0 atol=ε
+            @test MOI.cangetattribute(m, MOI.ConstraintDual(), vc2)
+            @test MOI.getattribute(m, MOI.ConstraintDual(), vc2) ≈ 1 atol=ε
+        end
+
+        # add new variable to get :
+        # max x + 2z
+        # s.t. x + y + z <= 1
+        # x,y,z >= 0
+
+        z = MOI.addvariable!(m)
+        push!(v, z)
+        @test v[3] == z
+
+        vc3 = MOI.addconstraint!(m, MOI.SingleVariable(v[3]), MOI.GreaterThan(0.0))
+        @test MOI.getattribute(m, MOI.NumberOfConstraints{MOI.SingleVariable,MOI.GreaterThan{Float64}}()) == 3
+
+        # MOI.modifyconstraint!(m, c, MOI.ScalarCoefficientChange{Float64}(z, 1.0))
+        # MOI.modifyobjective!(m, MOI.ScalarCoefficientChange{Float64}(z, 2.0))
+
+
+    end
+end
+
+linear1test(solver, ε)
