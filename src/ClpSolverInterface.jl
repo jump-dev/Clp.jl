b724ad9fccec1c8309521c6f6a03dc81d172d832
mbesancon <mathieu.besancon@gmail.com>
2018-08-02 13:31:02 +0200

    change scale -> rmul

--- src/ClpSolverInterface.jl
+++ src/ClpSolverInterface.jl
@@ -1,7 +1,7 @@
 module ClpMathProgSolverInterface
 using Clp.ClpCInterface
 
-using LinearAlgebra: scale!
+using LinearAlgebra: rmul!
 using SparseArrays: SparseMatrixCSC
 
 importall MathProgBase.SolverInterface
