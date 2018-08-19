287e24a3e93e34d410ae6adb9c0d0fcf7e7876c8
Miles Lubin <mlubin@google.com>
2018-08-18 23:22:24 -0400

    fix for Julia 0.6

--- test/mathprog.jl
+++ test/mathprog.jl
@@ -1,3 +1,9 @@
+# This is needed because both Pkg and MathProgBase export status :(.
+# Good thing MOI's tests aren't loaded via include.
+@static if VERSION < v"0.7-"
+    using MathProgBase: status
+end
+
 include(joinpath(Pkg.dir("MathProgBase"),"test","linprog.jl"))
 linprogtest(ClpSolver())
 
