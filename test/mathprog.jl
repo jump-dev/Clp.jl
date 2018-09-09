bf3cb7754753ea5dab58fadae51e16d16c9d4c46
Gunnar FarnebÃ¤ck <gunnar@lysator.liu.se>
2018-09-09 12:27:47 +0200

    Fix Pkg.dir deprecation.

--- test/mathprog.jl
+++ test/mathprog.jl
@@ -1,8 +1,14 @@
-include(joinpath(Pkg.dir("MathProgBase"),"test","linprog.jl"))
+if VERSION < v"0.7"
+    testdir = joinpath(Pkg.dir("MathProgBase"), "test")
+else
+    import MathProgBase
+    testdir = joinpath(dirname(pathof(MathProgBase)), "..", "test")
+end
+include(joinpath(testdir, "linprog.jl"))
 linprogtest(ClpSolver())
 
-include(joinpath(Pkg.dir("MathProgBase"),"test","linproginterface.jl"))
+include(joinpath(testdir, "linproginterface.jl"))
 linprogsolvertest(ClpSolver())
 
-include(joinpath(Pkg.dir("MathProgBase"),"test","conicinterface.jl"))
+include(joinpath(testdir, "conicinterface.jl"))
 coniclineartest(ClpSolver())
