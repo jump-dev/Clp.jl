8d9d95537078a5bafc97042f999d8b5f83fc38fc
Juan Pablo Vielma <jvielma@mit.edu>
2018-10-13 18:40:23 -0400

    Update build.jl

--- deps/build.jl
+++ deps/build.jl
@@ -82,12 +82,13 @@ products = [
     LibraryProduct(prefix, ["libClpSolver"], :libClpSolver),
 ]
 dependencies = [
-    "https://github.com/JuliaOpt/OsiBuilder/releases/download/v0.107.9-gccnoglpk-1/build_OsiBuilder.v0.107.9.jl",
-    "https://github.com/JuliaOpt/CoinUtilsBuilder/releases/download/v2.10.14-gccnoglpk/build_CoinUtilsBuilder.v2.10.14.jl",
+    "https://github.com/JuliaOpt/OsiBuilder/releases/download/bf/build_OsiBuilder.v0.107.9.jl",
+    "https://github.com/JuliaOpt/CoinUtilsBuilder/releases/download/bf/build_CoinUtilsBuilder.v2.10.14.jl",
     "https://github.com/JuliaOpt/COINMumpsBuilder/releases/download/v1.6.0-1/build_COINMumpsBuilder.v1.6.0.jl",
     "https://github.com/JuliaOpt/COINMetisBuilder/releases/download/v1.3.5-gcc/build_COINMetisBuilder.v1.3.5.jl",
-    "https://github.com/JuliaOpt/COINLapackBuilder/releases/download/v1.5.6-1/build_COINLapackBuilder.v1.5.6.jl",
-    "https://github.com/JuliaOpt/COINBLASBuilder/releases/download/v1.4.6-1/build_COINBLASBuilder.v1.4.6.jl",
+    "https://github.com/JuliaOpt/COINLapackBuilder/releases/downloadbf/build_COINLapackBuilder.v1.5.6.jl",
+    "https://github.com/JuliaOpt/COINBLASBuilder/releases/download/bf/build_COINBLASBuilder.v1.4.6.jl",
+    "https://github.com/JuliaOpt/COINGLPKBuilder/releases/download/bf/build_COINGLPKBuilder.v1.10.5.jl",
     "https://github.com/JuliaOpt/ASLBuilder/releases/download/v3.1.0-gcc/build_ASLBuilder.v3.1.0.jl"
 ]
 
