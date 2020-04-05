8374afd7d64271e1b175d629da67a5cd6520afda
odow <o.dowson@gmail.com>
2020-04-05 17:18:57 -0500

    Try single testset

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,10 +5,12 @@ using Compat
 using Compat.Test
 import Compat.Pkg
 
-@testset "MathProgBase" begin
-    include("mathprog.jl")
-end
+@testset "Clp" begin
+    @testset "MathProgBase" begin
+        include("mathprog.jl")
+    end
 
-@testset "MathOptInterface" begin
-    include("MOIWrapper.jl")
+    @testset "MathOptInterface" begin
+        include("MOIWrapper.jl")
+    end
 end
