7a5aa640626696ccfbac076c11c28536e0388d28
odow <o.dowson@gmail.com>
2020-04-07 09:13:32 -0500

    Rewrite bug in ccalls

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,15 +6,61 @@ import Compat.Pkg
 # @testset "MathProgBase" begin
 #     include("mathprog.jl")
 # end
-
-@testset "solve_unbounded_model (i)" begin
-    model = Clp.MOIU.CachingOptimizer(
-        Clp.MOIU.UniversalFallback(Clp.MOIU.Model{Float64}()),
-        Clp.Optimizer()
+const C = Clp.ClpCInterface
+function double_free_bug()
+    model = C.@clp_ccall(newModel, Ptr{Cvoid}, ())
+    options = C.@clpsolve_ccall(new, Ptr{Cvoid}, ())
+    C.@clp_ccall(
+        addColumns,
+        Cvoid,
+        (Ptr{Cvoid}, Int32, Ptr{Float64}, Ptr{Float64}, Ptr{Float64}, Ptr{Int32}, Ptr{Int32}, Ptr{Float64}),
+        model, 1, [-Inf], [Inf], [0.0], Int32[0, 0], Int32[], Float64[]
     )
-    config = Clp.MOI.Test.TestConfig()
-    Clp.MOI.Test.solve_unbounded_model(model, config)
+    C.@clp_ccall(
+        addColumns,
+        Cvoid,
+        (Ptr{Cvoid}, Int32, Ptr{Float64}, Ptr{Float64}, Ptr{Float64}, Ptr{Int32}, Ptr{Int32}, Ptr{Float64}),
+        model, 1, [-Inf], [Inf], [0.0], Int32[0, 0], Int32[], Float64[]
+    )
+    C.@clp_ccall(
+        addColumns,
+        Cvoid,
+        (Ptr{Cvoid}, Int32, Ptr{Float64}, Ptr{Float64}, Ptr{Float64}, Ptr{Int32}, Ptr{Int32}, Ptr{Float64}),
+        model, 1, [-Inf], [Inf], [0.0], Int32[0, 0], Int32[], Float64[]
+    )
+    C.@clp_ccall(
+        addColumns,
+        Cvoid,
+        (Ptr{Cvoid}, Int32, Ptr{Float64}, Ptr{Float64}, Ptr{Float64}, Ptr{Int32}, Ptr{Int32}, Ptr{Float64}),
+        model, 1, [-Inf], [Inf], [0.0], Int32[0, 0], Int32[], Float64[]
+    )
+    C.@clp_ccall(
+        addColumns,
+        Cvoid,
+        (Ptr{Cvoid}, Int32, Ptr{Float64}, Ptr{Float64}, Ptr{Float64}, Ptr{Int32}, Ptr{Int32}, Ptr{Float64}),
+        model, 1, [-Inf], [Inf], [0.0], Int32[0, 0], Int32[], Float64[]
+    )
+    C.@clp_ccall(setObjSense, Cvoid, (Ptr{Cvoid}, Float64), model, -1)
+    n = C.@clp_ccall(getNumCols, Cint, (Ptr{Cvoid},), model)
+    C.@clp_ccall(chgObjCoefficients, Cvoid, (Ptr{Cvoid}, Ptr{Float64}), model, fill(1.0, n))
+    C.@clp_ccall(setObjectiveOffset, Cvoid, (Ptr{Cvoid}, Float64), model, 0.0)
+    C.@clp_ccall(initialSolveWithOptions, Cint, (Ptr{Cvoid},Ptr{Cvoid}), model, options)
+    C.@clp_ccall(status, Cint, (Ptr{Cvoid},), model)
 end
+
+double_free_bug()
+double_free_bug()
+double_free_bug()
+double_free_bug()
+
+# @testset "solve_unbounded_model (i)" begin
+#     model = Clp.MOIU.CachingOptimizer(
+#         Clp.MOIU.UniversalFallback(Clp.MOIU.Model{Float64}()),
+#         Clp.Optimizer()
+#     )
+#     config = Clp.MOI.Test.TestConfig()
+#     Clp.MOI.Test.solve_unbounded_model(model, config)
+# end
 
 # @testset "solve_unbounded_model (ii)" begin
 #     model = Clp.MOIU.CachingOptimizer(
