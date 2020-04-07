111a4f0bfb58116209bf2dd152a48f2a0771d410
odow <o.dowson@gmail.com>
2020-04-07 08:56:38 -0500

    Isolate failing test

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,9 +3,9 @@ using Compat
 using Compat.Test
 import Compat.Pkg
 
-@testset "MathProgBase" begin
-    include("mathprog.jl")
-end
+# @testset "MathProgBase" begin
+#     include("mathprog.jl")
+# end
 
 @testset "solve_unbounded_model (i)" begin
     model = Clp.MOIU.CachingOptimizer(
@@ -16,24 +16,24 @@ end
     Clp.MOI.Test.solve_unbounded_model(model, config)
 end
 
-@testset "solve_unbounded_model (ii)" begin
-    model = Clp.MOIU.CachingOptimizer(
-        Clp.MOIU.UniversalFallback(Clp.MOIU.Model{Float64}()),
-        Clp.Optimizer()
-    )
-    config = Clp.MOI.Test.TestConfig()
-    Clp.MOI.Test.solve_unbounded_model(model, config)
-end
+# @testset "solve_unbounded_model (ii)" begin
+#     model = Clp.MOIU.CachingOptimizer(
+#         Clp.MOIU.UniversalFallback(Clp.MOIU.Model{Float64}()),
+#         Clp.Optimizer()
+#     )
+#     config = Clp.MOI.Test.TestConfig()
+#     Clp.MOI.Test.solve_unbounded_model(model, config)
+# end
 
-@testset "solve_unbounded_model (iii)" begin
-    model = Clp.MOIU.CachingOptimizer(
-        Clp.MOIU.UniversalFallback(Clp.MOIU.Model{Float64}()),
-        Clp.Optimizer()
-    )
-    config = Clp.MOI.Test.TestConfig()
-    Clp.MOI.Test.solve_unbounded_model(model, config)
-end
+# @testset "solve_unbounded_model (iii)" begin
+#     model = Clp.MOIU.CachingOptimizer(
+#         Clp.MOIU.UniversalFallback(Clp.MOIU.Model{Float64}()),
+#         Clp.Optimizer()
+#     )
+#     config = Clp.MOI.Test.TestConfig()
+#     Clp.MOI.Test.solve_unbounded_model(model, config)
+# end
 
-@testset "MathOptInterface" begin
-    include("MOIWrapper.jl")
-end
+# @testset "MathOptInterface" begin
+#     include("MOIWrapper.jl")
+# end
