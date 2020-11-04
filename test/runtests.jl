98c879bced41e5a4ead7cb9fc37e594edca402c7
Oscar Dowson <odow@users.noreply.github.com>
2020-11-04 15:38:19 +1300

    Refactor tests (#104)

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,16 @@
 using Test
 
+function runtests(mod)
+    for name in names(mod; all = true)
+        if !startswith("$(name)", "test_")
+            continue
+        end
+        @testset "$(name)" begin
+            getfield(mod, name)()
+        end
+    end
+end
+
 @testset "MathOptInterface" begin
     include("MOI_wrapper.jl")
 end
