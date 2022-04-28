c0d54e8d8e6d0f503bd36d5bac966b88229b9dbd
odow <o.dowson@gmail.com>
2022-04-29 11:37:18 +1200

    Add copyright headers

--- src/precompile.jl
+++ src/precompile.jl
@@ -1,3 +1,8 @@
+# Copyright (c) 2013: Clp.jl contributors
+#
+# Use of this source code is governed by an MIT-style license that can be found
+# in the LICENSE.md file or at https://opensource.org/licenses/MIT.
+
 function warmup()
     bridge = MOI.instantiate(Optimizer; with_bridge_type=Float64)
     no_bridge = MOI.instantiate(Optimizer)
