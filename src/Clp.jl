b22728c4c645123d6d5ad311692d62a7f92686e8
Oscar Dowson <o.dowson@gmail.com>
2025-09-23 09:43:38 +1200

    Fix formatting

--- src/Clp.jl
+++ src/Clp.jl
@@ -47,15 +47,12 @@ PrecompileTools.@setup_workload begin
     PrecompileTools.@compile_workload begin
         let
             model = MOI.Utilities.CachingOptimizer(
-                MOI.Utilities.UniversalFallback(
-                    MOI.Utilities.Model{Float64}(),
-                ),
+                MOI.Utilities.UniversalFallback(MOI.Utilities.Model{Float64}()),
                 MOI.instantiate(Clp.Optimizer; with_bridge_type = Float64),
             )
             MOI.set(model, MOI.Silent(), true)
             x = MOI.add_variables(model, 3)
-            sets =
-                (MOI.GreaterThan(0.0), MOI.LessThan(2.0), MOI.EqualTo(1.0))
+            sets = (MOI.GreaterThan(0.0), MOI.LessThan(2.0), MOI.EqualTo(1.0))
             for i in 1:3, f in (x[i], 1.0 * x[1] + 2.0 * x[2])
                 MOI.supports_constraint(model, typeof(f), typeof(sets[i]))
                 MOI.add_constraint(model, f, sets[i])
