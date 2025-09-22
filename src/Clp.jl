9b7c630fae02917f952edc0e80057fae8445687b
Oscar Dowson <odow@users.noreply.github.com>
2025-09-23 10:01:32 +1200

    Add support for aarch64-apple-darwin (#162)

--- src/Clp.jl
+++ src/Clp.jl
@@ -11,23 +11,16 @@ import MathOptInterface as MOI
 import OpenBLAS32_jll
 
 function __init__()
-    @static if Sys.isapple() && Sys.ARCH == :aarch64
-        error(
-            "Clp.jl does not support Apple M-series processors. Please use HiGHS.jl instead.",
-        )
-    end
-    if VERSION >= v"1.9"
-        config = LinearAlgebra.BLAS.lbt_get_config()
-        if !any(lib -> lib.interface == :lp64, config.loaded_libs)
-            LinearAlgebra.BLAS.lbt_forward(OpenBLAS32_jll.libopenblas_path)
-        end
+    config = LinearAlgebra.BLAS.lbt_get_config()
+    if !any(lib -> lib.interface == :lp64, config.loaded_libs)
+        LinearAlgebra.BLAS.lbt_forward(OpenBLAS32_jll.libopenblas_path)
     end
     version = VersionNumber(
         Clp_VersionMajor(),
         Clp_VersionMinor(),
         Clp_VersionRelease(),
     )
-    if !(v"1.17.2" <= version <= v"1.17.9")
+    if !(v"1.17.2" <= version <= v"1.17.10")
         error(
             "You have installed version $version of Clp, which is not " *
             "supported by Clp.jl. If the version change was breaking, changes " *
@@ -50,36 +43,31 @@ end
 
 import PrecompileTools
 
-@static if !(Sys.isapple() && Sys.ARCH == :aarch64)
-    PrecompileTools.@setup_workload begin
-        PrecompileTools.@compile_workload begin
-            let
-                model = MOI.Utilities.CachingOptimizer(
-                    MOI.Utilities.UniversalFallback(
-                        MOI.Utilities.Model{Float64}(),
-                    ),
-                    MOI.instantiate(Clp.Optimizer; with_bridge_type = Float64),
-                )
-                MOI.set(model, MOI.Silent(), true)
-                x = MOI.add_variables(model, 3)
-                sets =
-                    (MOI.GreaterThan(0.0), MOI.LessThan(2.0), MOI.EqualTo(1.0))
-                for i in 1:3, f in (x[i], 1.0 * x[1] + 2.0 * x[2])
-                    MOI.supports_constraint(model, typeof(f), typeof(sets[i]))
-                    MOI.add_constraint(model, f, sets[i])
-                end
-                f = 1.0 * x[1] + x[2] + x[3]
-                MOI.set(model, MOI.ObjectiveSense(), MOI.MAX_SENSE)
-                MOI.supports(model, MOI.ObjectiveFunction{typeof(f)}())
-                MOI.set(model, MOI.ObjectiveFunction{typeof(f)}(), f)
-                MOI.optimize!(model)
-                MOI.get(model, MOI.TerminationStatus())
-                MOI.get(model, MOI.PrimalStatus())
-                MOI.get(model, MOI.DualStatus())
-                MOI.get(model, MOI.VariablePrimal(), x)
+PrecompileTools.@setup_workload begin
+    PrecompileTools.@compile_workload begin
+        let
+            model = MOI.Utilities.CachingOptimizer(
+                MOI.Utilities.UniversalFallback(MOI.Utilities.Model{Float64}()),
+                MOI.instantiate(Clp.Optimizer; with_bridge_type = Float64),
+            )
+            MOI.set(model, MOI.Silent(), true)
+            x = MOI.add_variables(model, 3)
+            sets = (MOI.GreaterThan(0.0), MOI.LessThan(2.0), MOI.EqualTo(1.0))
+            for i in 1:3, f in (x[i], 1.0 * x[1] + 2.0 * x[2])
+                MOI.supports_constraint(model, typeof(f), typeof(sets[i]))
+                MOI.add_constraint(model, f, sets[i])
             end
+            f = 1.0 * x[1] + x[2] + x[3]
+            MOI.set(model, MOI.ObjectiveSense(), MOI.MAX_SENSE)
+            MOI.supports(model, MOI.ObjectiveFunction{typeof(f)}())
+            MOI.set(model, MOI.ObjectiveFunction{typeof(f)}(), f)
+            MOI.optimize!(model)
+            MOI.get(model, MOI.TerminationStatus())
+            MOI.get(model, MOI.PrimalStatus())
+            MOI.get(model, MOI.DualStatus())
+            MOI.get(model, MOI.VariablePrimal(), x)
         end
     end
 end
 
-end
+end  # module
